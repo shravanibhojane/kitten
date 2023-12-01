93fccc9ebe6ecad3bfbbef745edbdb438a677d7d
Shravani <shravanibhojane21@gmail.com>
2023-12-01 17:44:48 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shravanibhojane/kitten
 
 go 1.21.1
+
+require github.com/shravanibhojane/cat v0.0.0-20231201120344-485402c98006 // indirect
--- kitten.go
+++ kitten.go
@@ -1,9 +1,20 @@
 package kitten
 
+import (
+	"github.com/shravanibhojane/cat"
+)
+
 func Bark() string {
 	return "Meow!"
 }
 
 func Barks() string {
 	return "Meow!Meow!Meow!"
+}
+
+func BigBark() string {
+	return cat.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return cat.WhenGrownUp(Barks())
 }
